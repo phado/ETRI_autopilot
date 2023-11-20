4d2b0f20b0e155140623917b3892ad38458d06ac
minsoo <bellamy122@naver.com>
2023-11-20 22:56:27 +0900

    일단 커

--- app.py
+++ app.py
@@ -2,7 +2,25 @@ from flask import Flask, request, render_template
 from db_conn import get_pool_conn
 app = Flask(__name__)
 
-mariadb_pool = get_pool_conn()
+# mariadb_pool = get_pool_conn()
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("login.html")
+
+@app.route('/findId')
+def findId():
+    return render_template("")
+
+@app.route('/findPwd')
+def findId():
+    return render_template("")
+
+
 
 @app.route('/dataManagement')
 def dataManagement():
